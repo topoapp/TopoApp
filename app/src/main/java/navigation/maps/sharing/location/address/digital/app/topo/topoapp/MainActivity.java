7202a90dba9192aeff71a3a5e259ab96d95b0222
topo <contact@topoapp.in>
2018-05-08 15:04:15 +0530

    added build files

---
+++ app/src/main/java/navigation/maps/sharing/location/address/digital/app/topo/topoapp/MainActivity.java
@@ -0,0 +1,40 @@
+package navigation.maps.sharing.location.address.digital.app.topo.topoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import navigation.maps.sharing.location.address.digital.app.topo.mylibrary.TopoInitialization;
+import navigation.maps.sharing.location.address.digital.app.topo.mylibrary.holder.TopoData;
+import navigation.maps.sharing.location.address.digital.app.topo.mylibrary.notifier.TopoAppNotifier;
+
+public class MainActivity extends AppCompatActivity implements TopoAppNotifier {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gototopo = (Button) findViewById(R.id.gototopo);
+        gototopo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TopoInitialization.getInstance(MainActivity.this).callingTopoHome(MainActivity.this, "35c6af907ceb7b5df8e2a1f321d82fb5", "7022858863", "91");
+                TopoInitialization.getInstance(MainActivity.this).setAppNotifier(MainActivity.this);
+            }
+        });
+
+    }
+
+    @Override
+    public void topoAppSucess(TopoData data) {
+        Toast.makeText(MainActivity.this, "Topo Data is Got" + data.getTopo_name(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void topoAppFailure() {
+
+    }
+}
